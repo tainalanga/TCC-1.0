75efed7652aea233ae72a027e69481fd0986de81
tainalanga <langataina@gmail.com>
2018-07-03 09:31:08 -0300

    Add files via upload

--- TCC/TCCfuncionando-master/app/controller/usuario.php
+++ TCC/TCCfuncionando-master/app/controller/usuario.php
@@ -15,7 +15,7 @@
 
         public function checarLogin(){
             // if blablabla
-            $this->tpl->CONTENT = $this->templates['dashboard'];
+            $this->tpl->CONTENT = $this->templates['perfil'];
         }
     }
 
